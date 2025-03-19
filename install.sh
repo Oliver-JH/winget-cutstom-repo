195e16b7f3e32f0b9ea0c86cc03bf2d250139fa6
Oliver-JH <168336410+Oliver-JH@users.noreply.github.com>
2025-03-19 17:51:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 xcode-select --install
 
+# Homebrew 설치 확인 및 설치
 if ! command -v brew &>/dev/null; then
     echo "Homebrew 설치 중..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
+# Homebrew 버전 확인
 brew -v
 
-# 기본 소프트웨어 자동 설치
+# 예시: 소프트웨어 설치
 brew install --cask google-chrome
 brew install --cask forticlient
 
-# 시스템 설정 자동 변경 (예제)
 defaults write com.apple.dock autohide -bool true
 killall Dock
